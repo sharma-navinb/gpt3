6aa3638a84ca89f53f67c0cac457f706ede50012
Navin Sharma <navinsharmasuprb@gmail.com>
2023-09-03 02:11:59 +0530

    Navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Footer, Blog, Header, Features, WhatGPT3, Possibility } from './containers';
+import { Navbar, Feature, CTA, Brand, Article } from './components';
+import './App.css'
+import './index.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+        
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
